4fb7c4428c9a99db8fac65882e97a7e2149ad6b1
Long <longtrong53@gmail.com>
2024-03-17 22:53:55 +0700

    create models and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+AUTH_USER_MODEL = "profiles_api.UserProfile"  # Add the AUTH_USER_MODEL setting to the settings.py file
